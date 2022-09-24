5c621a934644bfc867384dd96db2b06de904fb16
unknown <cabaraul@gmail.com>
2022-09-24 08:49:27 -0400

    working proyect

---
+++ src/Mole.js
@@ -0,0 +1,11 @@
+// Just one example
+import Mole from './Mole'
+
+export function Mole(props){
+    return (
+        <div>
+            <h2> Mole  </h2>
+            <Mole />
+        </div>
+    )
+}
---
+++ src/MoleContainer.js
@@ -0,0 +1,11 @@
+// Just one example
+import Mole from './Mole'
+
+export function MoleContainer(props){
+    return (
+        <div>
+            <h2> Mole Container </h2>
+            <Mole />
+        </div>
+    )
+}
